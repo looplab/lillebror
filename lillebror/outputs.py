be7b8ae5f196a4cb6e71806869d35a834e13b83a
Max Persson <max@looplab.se>
2013-05-10 16:57:56 +0200

    Restructure lillebror to a more modular aproach
    Each sampler is in it's own class and each output is in it's own class
    to make it easier to add new functionality. Two factor methods,
    make_sampler and make_output, are responsible for creating the actual
    samplers and outputs and link them together.

---
+++ lillebror/outputs.py
@@ -0,0 +1,114 @@
+# Copyright 2013 Loop Lab
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+import os
+import datetime
+import csv
+
+import gevent
+import zerorpc
+
+
+def make_output(dct, all_samplers):
+    samplers = {}
+    for name, sampler in all_samplers.iteritems():
+        # TODO: Needs to be added in order here
+        if (not 'samplers' in dct) or (name in dct['samplers']):
+            samplers[name] = sampler
+
+    output = None
+    if dct['type'] == 'csv':
+        output = CSVOutput(samplers, dct['path'], dct['start_time'])
+    elif dct['type'] == 'zerorpc':
+        output = ZeroRPCOutput(samplers, dct['url'])
+
+    return output
+
+
+class BaseOutput(object):
+    def __init__(self, samplers):
+        self._samplers = samplers
+        self._kill_flag = False
+        self._output_task = None
+
+    def start(self):
+        self._output_task = gevent.spawn(self._output)
+        self._output_task.link(self._stop)
+
+    def stop(self):
+        self._kill_flag = True
+
+    def _stop(self, task):
+        pass
+
+    def _output(self):
+        while not self._kill_flag:
+            for name, sampler in self._samplers.iteritems():
+                sampler.sample()
+            self._do_output()
+            gevent.sleep(1.0)
+
+    def _do_output(self):
+        raise NotImplementedError
+
+
+class CSVOutput(BaseOutput):
+    def __init__(self, samplers, path, start_time):
+        super(CSVOutput, self).__init__(samplers)
+        self._path = path
+        self._start_time = start_time
+        self._file = None
+        self._writer = None
+
+    def start(self):
+        if self._path:
+            self._file = open(self._path, 'w')
+            self._writer = csv.writer(self._file)
+            self._writer.writerow(('time', 'cpu', 'memory', 'task_switches'))
+            self._file.flush()
+            os.fsync(self._file.fileno())
+        super(CSVOutput, self).start()
+
+    def _stop(self, task):
+        if self._file:
+            self._file.close()
+
+    def _do_output(self):
+        time = datetime.datetime.now() - self._start_time
+        values = [int(time.total_seconds())]
+        for name, sampler in self._samplers.iteritems():
+            values.append(sampler.value)
+
+        if self._path:
+            self._writer.writerow(values)
+            self._file.flush()
+            os.fsync(self._file.fileno())
+
+
+class ZeroRPCOutput(BaseOutput):
+    def __init__(self, samplers, url):
+        super(ZeroRPCOutput, self).__init__(samplers)
+        self._url = url
+        self._client = zerorpc.Client()
+
+    def start(self):
+        self._client.connect(self._url)
+        super(ZeroRPCOutput, self).start()
+
+    def _do_output(self):
+        dct = {}
+        for name, sampler in self._samplers.iteritems():
+            dct[name] = sampler.value
+        self._client.set_sample(dct)
---
+++ lillebror/samplers.py
@@ -0,0 +1,130 @@
+# Copyright 2013 Loop Lab
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+import os
+import collections
+
+import psutil
+# import gevent.subprocess
+
+
+def make_sampler(dct):
+    sampler = None
+    if dct['type'] == 'python':
+        sampler = PythonSampler(dct['code'])
+    elif dct['type'] == 'cpu':
+        sampler = CPUSampler(dct.get('pid', None))
+    elif dct['type'] == 'memory':
+        sampler = MemorySampler(dct.get('pid', None))
+    elif dct['type'] == 'task_switches':
+        sampler = SwitchSampler(dct.get('pid', None))
+
+    return sampler
+
+
+class BaseSampler(object):
+    def __init__(self):
+        self._samples = collections.deque(maxlen=10)
+        self.value = 0.0
+
+    def sample(self):
+        self._calc_average()
+
+    def _calc_average(self):
+        if self._samples:
+            s = sum(self._samples)
+            self.value = s / float(len(self._samples))
+
+
+class PythonSampler(BaseSampler):
+    def __init__(self, code):
+        super(PythonSampler, self).__init__()
+        self._code = code
+
+    def sample(self):
+        self.value = eval(self._code)
+        super(PythonSampler, self).sample()
+
+
+class CommandSampler(BaseSampler):
+    def __init__(self, cmd):
+        super(CommandSampler, self).__init__()
+        self._cmd = cmd
+
+    def sample(self):
+        # TODO: implement command call here
+        super(CommandSampler, self).sample()
+
+
+class ProcessSampler(BaseSampler):
+    def __init__(self, pid=None):
+        super(ProcessSampler, self).__init__()
+        self._pid = pid
+
+        if not self._pid:
+            self._pid = os.getpid()
+
+        self._process = None
+        try:
+            self._process = psutil.Process(self._pid)
+        except psutil.NoSuchProcess:
+            return
+
+    def sample(self):
+        try:
+            child_processes = self._process.get_children(recursive=True)
+        except psutil.NoSuchProcess:
+            return
+        child_processes.append(self._process)
+
+        s = 0.0
+        for p in child_processes:
+            new_s = self._get_sample(p)
+            if new_s:
+                s += new_s
+
+        self._samples.append(s)
+        super(ProcessSampler, self).sample()
+
+
+class CPUSampler(ProcessSampler):
+    def _get_sample(self, p):
+        try:
+            return p.get_cpu_percent(0)
+        except psutil.AccessDenied:
+            pass
+
+
+class MemorySampler(ProcessSampler):
+    def _get_sample(self, p):
+        try:
+            return p.get_memory_info()[0]
+        except psutil.AccessDenied:
+            pass
+
+
+class SwitchSampler(ProcessSampler):
+    def __init__(self, sampler):
+        super(SwitchSampler, self).__init__(sampler)
+        self._prev = 0
+
+    def _get_sample(self, p):
+        try:
+            sample = p.get_num_ctx_switches()[0]
+            new = sample - self._prev
+            self._prev = sample
+            return new
+        except psutil.AccessDenied:
+            pass
